d7eb3aca092c29eb743a4fca1640d4107b983998
Rommel Rico <me@rommelrico.com>
2022-07-30 12:50:54 -0700

    :white_check_mark: ScalaCheck: BasicScalaCheckProperties passing

--- src/test/scala/BasicScalaCheckProperties.scala
+++ src/test/scala/BasicScalaCheckProperties.scala
@@ -3,5 +3,5 @@ package com.rommelrico.testinginscala
 import org.scalacheck.{Prop, Properties}
 
 object BasicScalaCheckProperties extends Properties("Simple Math") {
-  property("Sum is greater than its parts") = Prop.forAll({(x: Int, y: Int) => x + y > x && x + y > y})
+  property("Sums are associative") = Prop.forAll({(x: Int, y: Int) => x + y == y + x})
 }
